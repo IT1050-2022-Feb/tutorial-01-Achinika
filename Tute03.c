272e598c5440bfd69e60aaf95e0116ad9c099526
A.K Arangala <95092298+Achinika@users.noreply.github.com>
2022-02-14 21:39:16 +0530

    Update Tute03.c

--- Tute03.c
+++ Tute03.c
@@ -2,18 +2,36 @@
 
 Write a C program to calculate the sum of the numbers from 1 to n.
 Where n is a keyboard input.
-
 e.g.
-
 n -> 100
 sum = 1+2+3+....+ 99+100 = 5050
 
 n -> 1-
 sum = 1+2+3+...+10 = 55 */
 
+
 #include <stdio.h>
-int main() {
+int main(void) //main program execution begins
+{
+  //variable declaration
+  int no;
+  int sum = 0; //initialization
+  int i; //counter
+  
+  //input
+  printf (" Enter a positive integer number/x: ");
+  scanf (" %d ", &no );
+  
+  //calculations
+  for ( i = 1; i <= no; ++i ) 
+  {
+    sum += i;
+  } //for loop ends
+  
+  //output
+  printf (" \n\nSUM of the Numbers from 1 to x is %d ", sum);
   
   return 0;
-}
+  
+}//main program execution ends
 
